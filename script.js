f29e13f0291662645e0b0ed8d09e5af9f8f69378
Cyber <44707958+CyberL1@users.noreply.github.com>
2021-08-08 01:43:29 +0200

    Switch to iframe

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const _DiscordBotDemo_Script = document.currentScript;
 
-document.write(`<script src="https://unpkg.com/wc-discord-message@2.0.4/dist/wc-discord-message/wc-discord-message.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://discord-bot-demo.github.io/assets/main.css">
-    <script src="https://discord-bot-demo.github.io/assets/main.js"></script>
+const width = _DiscordBotDemo_Script.getAttribute('width');
+const height = _DiscordBotDemo_Script.getAttribute('height');
+const commandHandler = _DiscordBotDemo_Script.getAttribute('command_handler');
+const beginning_message = _DiscordBotDemo_Script.getAttribute('beginning_message');
+const user_username = _DiscordBotDemo_Script.getAttribute('user_username');
+const user_avatar = _DiscordBotDemo_Script.getAttribute('user_avatar');
+const bot_username = _DiscordBotDemo_Script.getAttribute('bot_username');
+const bot_avatar = _DiscordBotDemo_Script.getAttribute('bot_avatar');
 
-    <div class="discord-bot-demo">
-        <div id="main">
-            <div id="inner">
-            <div id="channels"></div>
-            <div id="channel-area">
-            <div id="messages"><discord-messages></discord-messages></div>
-                <div id="text-area">
-                <div id="mention-box"></div>
-                <div id="text-box">
-                    <div contenteditable="true" id="input"></div>
-                </div>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>`);
+let url = 'https://discord-bot-demo.github.io/demo.html';
+
+const querySign = url.includes('?') ? '&' : '?';
+if (user_username !== null) url += `${querySign}user_username=${user_username}`;
+if (user_avatar !== null) url += `${querySign}user_avatar=${user_avatar}`;
+if (bot_username !== null) url += `${querySign}bot_username=${bot_username}`;
+if (bot_avatar !== null) url += `${querySign}bot_avatar=${bot_avatar}`;
+if (width !== null) url += `${querySign}width=${width}`;
+if (height !== null) url += `${querySign}height=${height}`;
+if (beginning_message !== null) url += `${querySign}msg=${beginning_message}`;
+
+document.write(`<iframe id="DiscordBotDemoFrame" width="100%" height="100%" frameBorder="0" scrolling="no" src="${url}"></iframe>`);
