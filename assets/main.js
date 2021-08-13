55c4d6400ebfc72883574864546d48947eaa0411
Cyber <44707958+CyberL1@users.noreply.github.com>
2021-08-13 16:33:29 +0200

    Add channel mentions

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,4 @@
-/*
- * Custom log function
-*/
-
-log = function(type, message) {
-    message = `[Discord Bot Demo] ${message}`;
-    console[type](message);
-}
-
-/*
- * Data sending
-*/
-
-//-----------//
-// Defaults //
-//---------//
-
-const defaultUserAvatars = [
+const _defaultUserAvatars = [
     'https://cdn.discordapp.com/embed/avatars/0.png',
     'https://cdn.discordapp.com/embed/avatars/1.png',
     'https://cdn.discordapp.com/embed/avatars/2.png',
@@ -24,31 +7,54 @@ const defaultUserAvatars = [
     'https://cdn.discordapp.com/embed/avatars/5.png'
 ]
 
-const defaults = {
+const _ = {
     avatars: {
-        user: defaultUserAvatars[Math.floor(Math.random() * defaultUserAvatars.length)],
+        user: _defaultUserAvatars[Math.floor(Math.random() * _defaultUserAvatars.length)],
         bot: 'https://discord.com/assets/f78426a064bc9dd24847519259bc42af.png'
     },
     usernames: {
         user: 'User',
         bot: 'Bot'
+    },
+    channelIcons: {
+        text: `<svg id="icon">
+        <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M5.88657 21C5.57547 21 5.3399 20.7189 5.39427 20.4126L6.00001 17H2.59511C2.28449 17 2.04905 16.7198 2.10259 16.4138L2.27759 15.4138C2.31946 15.1746 2.52722 15 2.77011 15H6.35001L7.41001 9H4.00511C3.69449 9 3.45905 8.71977 3.51259 8.41381L3.68759 7.41381C3.72946 7.17456 3.93722 7 4.18011 7H7.76001L8.39677 3.41262C8.43914 3.17391 8.64664 3 8.88907 3H9.87344C10.1845 3 10.4201 3.28107 10.3657 3.58738L9.76001 7H15.76L16.3968 3.41262C16.4391 3.17391 16.6466 3 16.8891 3H17.8734C18.1845 3 18.4201 3.28107 18.3657 3.58738L17.76 7H21.1649C21.4755 7 21.711 7.28023 21.6574 7.58619L21.4824 8.58619C21.4406 8.82544 21.2328 9 20.9899 9H17.41L16.35 15H19.7549C20.0655 15 20.301 15.2802 20.2474 15.5862L20.0724 16.5862C20.0306 16.8254 19.8228 17 19.5799 17H16L15.3632 20.5874C15.3209 20.8261 15.1134 21 14.8709 21H13.8866C13.5755 21 13.3399 20.7189 13.3943 20.4126L14 17H8.00001L7.36325 20.5874C7.32088 20.8261 7.11337 21 6.87094 21H5.88657ZM9.41045 9L8.35045 15H14.3504L15.4104 9H9.41045Z">
+        </path>
+        </svg>`,
+        voice: `<svg id="icon">
+        <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M11.383 3.07904C11.009 2.92504 10.579 3.01004 10.293 3.29604L6 8.00204H3C2.45 8.00204 2 8.45304 2 9.00204V15.002C2 15.552 2.45 16.002 3 16.002H6L10.293 20.71C10.579 20.996 11.009 21.082 11.383 20.927C11.757 20.772 12 20.407 12 20.002V4.00204C12 3.59904 11.757 3.23204 11.383 3.07904ZM14 5.00195V7.00195C16.757 7.00195 19 9.24595 19 12.002C19 14.759 16.757 17.002 14 17.002V19.002C17.86 19.002 21 15.863 21 12.002C21 8.14295 17.86 5.00195 14 5.00195ZM14 9.00195C15.654 9.00195 17 10.349 17 12.002C17 13.657 15.654 15.002 14 15.002V13.002C14.551 13.002 15 12.553 15 12.002C15 11.451 14.551 11.002 14 11.002V9.00195Z">
+        </path>
+        </svg>`
+    },
+    channels: {
+        text: 'Text',
+        voice: 'Voice'
     }
 }
 
-//--------------//
-// Actual data //
-//------------//
+/*
+ * Custom log function
+*/
+
+function log(type, message) {
+    message = `[Discord Bot Demo] ${message}`;
+    console[type](message);
+}
+
+/*
+ * Data sending
+*/
 
 const params = new URLSearchParams(location.search);
 
 const data = {
     user: {
-        username: params.get('uu') ? decodeURIComponent(params.get('uu')) : defaults.usernames.user,
-        avatar: params.get('ua') ? decodeURI(params.get('ua')) : defaults.avatars.user
+        username: params.get('uu') ? decodeURIComponent(params.get('uu')) : _.usernames.user,
+        avatar: params.get('ua') ? decodeURI(params.get('ua')) : _.avatars.user
     },
     bot: {
-        username: params.get('bu') ? decodeURI(params.get('bu')) : defaults.usernames.bot,
-        avatar: params.get('ba') ? decodeURI(params.get('ba')) : defaults.avatars.bot
+        username: params.get('bu') ? decodeURI(params.get('bu')) : _.usernames.bot,
+        avatar: params.get('ba') ? decodeURI(params.get('ba')) : _.avatars.bot
     },
     commandHandler: params.get('c') ? decodeURI(params.get('c')) : 'commandHandler',
     beginning_message: params.get('m') ? decodeURI(params.get('m')) : '',
@@ -108,18 +114,12 @@ function load() {
     const channelsDiv = document.querySelector('#channels');
     
     channelsDiv.innerHTML = `<div id="text">
-        <svg id="icon">
-        <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M5.88657 21C5.57547 21 5.3399 20.7189 5.39427 20.4126L6.00001 17H2.59511C2.28449 17 2.04905 16.7198 2.10259 16.4138L2.27759 15.4138C2.31946 15.1746 2.52722 15 2.77011 15H6.35001L7.41001 9H4.00511C3.69449 9 3.45905 8.71977 3.51259 8.41381L3.68759 7.41381C3.72946 7.17456 3.93722 7 4.18011 7H7.76001L8.39677 3.41262C8.43914 3.17391 8.64664 3 8.88907 3H9.87344C10.1845 3 10.4201 3.28107 10.3657 3.58738L9.76001 7H15.76L16.3968 3.41262C16.4391 3.17391 16.6466 3 16.8891 3H17.8734C18.1845 3 18.4201 3.28107 18.3657 3.58738L17.76 7H21.1649C21.4755 7 21.711 7.28023 21.6574 7.58619L21.4824 8.58619C21.4406 8.82544 21.2328 9 20.9899 9H17.41L16.35 15H19.7549C20.0655 15 20.301 15.2802 20.2474 15.5862L20.0724 16.5862C20.0306 16.8254 19.8228 17 19.5799 17H16L15.3632 20.5874C15.3209 20.8261 15.1134 21 14.8709 21H13.8866C13.5755 21 13.3399 20.7189 13.3943 20.4126L14 17H8.00001L7.36325 20.5874C7.32088 20.8261 7.11337 21 6.87094 21H5.88657ZM9.41045 9L8.35045 15H14.3504L15.4104 9H9.41045Z">
-        </path>
-        </svg>
-        <p id="name">Text Channel</p>
+        ${_.channelIcons.text}
+        <p id="name">${_.channels.text}</p>
     </div>
     <div id="voice">
-        <svg id="icon">
-        <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M11.383 3.07904C11.009 2.92504 10.579 3.01004 10.293 3.29604L6 8.00204H3C2.45 8.00204 2 8.45304 2 9.00204V15.002C2 15.552 2.45 16.002 3 16.002H6L10.293 20.71C10.579 20.996 11.009 21.082 11.383 20.927C11.757 20.772 12 20.407 12 20.002V4.00204C12 3.59904 11.757 3.23204 11.383 3.07904ZM14 5.00195V7.00195C16.757 7.00195 19 9.24595 19 12.002C19 14.759 16.757 17.002 14 17.002V19.002C17.86 19.002 21 15.863 21 12.002C21 8.14295 17.86 5.00195 14 5.00195ZM14 9.00195C15.654 9.00195 17 10.349 17 12.002C17 13.657 15.654 15.002 14 15.002V13.002C14.551 13.002 15 12.553 15 12.002C15 11.451 14.551 11.002 14 11.002V9.00195Z">
-        </path>
-        </svg>
-        <p id="name">Voice Channel</p>
+        ${_.channelIcons.voice}
+        <p id="name">${_.channels.voice}</p>
     </div>
     <div class="member-list"></div>
     <div class="voice-controls">
@@ -144,32 +144,51 @@ function load() {
     const voiceControls = document.querySelector('#channels .voice-controls .content');
     const vcButton = document.querySelector('#channels .voice-controls .content button');
 
-    mentionBox.visible = mode => {
-        mode ? mentionBox.innerHTML = `<div id="title">members</div><div id="content" type="bot" selected="true"><img draggable="false" src="${data.bot.avatar}"> ${data.bot.username}</div><div id="content" type="user"><img draggable="false" src="${data.user.avatar}"> ${data.user.username}</div>` : mentionBox.innerHTML = '';
+    mentionBox.visible = (mode, type) => {
+        mode ? mentionBox.innerHTML = type === 'members' ? `<div id="title">members</div><div id="content" type="bot" selected="true"><div id="avatar"><img draggable="false" src="${data.bot.avatar}"></div><div id="text">${data.bot.username}</div></div></div><div id="content" type="user"><div id="avatar"><img draggable="false" src="${data.user.avatar}"></div><div id="text">${data.user.username}</div></div>` : `<div id="title">channels</div><div id="content" type="text" selected="true"><div id="avatar">${_.channelIcons.text}</div><div id="text">${_.channels.text}</div></div><div id="content" type="voice"><div id="avatar">${_.channelIcons.voice}</div><div id="text">${_.channels.voice}</div></div>` : mentionBox.innerHTML = '';
+    }
+
+    mentionBox.isVisible = () => {
+        return document.querySelector('#channel-area #text-area #mention-box').innerHTML !== '';
     }
 
     if (data.beginning_message) createMessage(true, data.beginning_message.trim());
 
     inputBox.addEventListener('keydown', e => {
         if (e.key === 'Enter') e.preventDefault();
-        if (e.key === '@') mentionBox.visible(true);
 
-        if (e.key === 'Backspace' && /.@$/.test(inputBox.textContent)) return mentionBox.visible(true);
-        if (e.key === 'Backspace' && /@$/.test(inputBox.textContent)) return mentionBox.visible(false);
-        if (e.key === 'Backspace' && /@.$/.test(inputBox.textContent)) return mentionBox.visible(true);
-    
-        if (/@$/.test(inputBox.textContent)) {
+        if (e.key === '@') {
+            _DiscordBotDemo_mentionBox_type = 'members';
+            mentionBox.visible(true, 'members');
+        }
+
+        if (e.key === '#') {
+            _DiscordBotDemo_mentionBox_type = 'channels';
+            mentionBox.visible(true, 'channels');
+        }
+        
+        if (/[#@]$/.test(inputBox.textContent) && e.key === 'Backspace' || e.ctrlKey && e.key === 'Backspace') return mentionBox.visible(false);
+        if (/.[#@]$|[#@].$/.test(inputBox.textContent) && e.key === 'Backspace' || e.ctrlKey && e.key === 'Backspace') {
+            if (/@/.test(inputBox.textContent)) _DiscordBotDemo_mentionBox_type = 'members';
+            if (/#/.test(inputBox.textContent)) _DiscordBotDemo_mentionBox_type = 'channels';
+            return mentionBox.visible(true, _DiscordBotDemo_mentionBox_type);
+        }
+        
+        if (/@$/.test(inputBox.textContent) && mentionBox.isVisible()) {
+            if (['Control', 'Shift', 'Alt', 'Delete', 'Insert', 'ScrollLock'].includes(e.key)) return;
+            if (!['Enter', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) return mentionBox.visible(false);
+
             const user = document.querySelector('#channel-area #text-area #mention-box #content[type="user"]');
             const bot = document.querySelector('#channel-area #text-area #mention-box #content[type="bot"]');
             const selected = document.querySelector('#channel-area #text-area #mention-box #content[selected="true"]');
 
-            if (e.key === 'ArrowUp') {
+            if (['ArrowUp', 'ArrowLeft'].includes(e.key)) {
                 e.preventDefault();
                 user.setAttribute('selected', 'false');
                 bot.setAttribute('selected', 'true');
             }
 
-            if (e.key === 'ArrowDown') {
+            if (['ArrowDown', 'ArrowRight'].includes(e.key)) {
                 e.preventDefault();
                 bot.setAttribute('selected', 'false');
                 user.setAttribute('selected', 'true');
@@ -177,7 +196,7 @@ function load() {
 
             if (e.key === 'Enter' || e.keyShift && e.key === 'Enter') {
                 mentionBox.visible(false);
-                inputBox.textContent = inputBox.textContent.replace(/@$/, `@${selected.textContent.trimStart()}`);
+                inputBox.textContent = inputBox.textContent.replace(/@$/, `@${selected.textContent}`);
                 const selection = window.getSelection();
                 const range = document.createRange();
                 selection.removeAllRanges();
@@ -188,6 +207,39 @@ function load() {
             }
         }
 
+        if (/#$/.test(inputBox.textContent) && mentionBox.isVisible()) {
+            if (['Control', 'Shift', 'Alt', 'Delete', 'Insert', 'ScrollLock'].includes(e.key)) return;
+            if (!['Enter', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) return mentionBox.visible(false);
+
+            const text = document.querySelector('#channel-area #text-area #mention-box #content[type="text"]');
+            const voice = document.querySelector('#channel-area #text-area #mention-box #content[type="voice"]');
+            const selected = document.querySelector('#channel-area #text-area #mention-box #content[selected="true"]');
+
+            if (['ArrowUp', 'ArrowLeft'].includes(e.key)) {
+                e.preventDefault();
+                voice.setAttribute('selected', 'false');
+                text.setAttribute('selected', 'true');
+            }
+
+            if (['ArrowDown', 'ArrowRight'].includes(e.key)) {
+                e.preventDefault();
+                text.setAttribute('selected', 'false');
+                voice.setAttribute('selected', 'true');
+            }
+
+            if (e.key === 'Enter' || e.keyShift && e.key === 'Enter') {
+                mentionBox.visible(false);
+                inputBox.textContent = inputBox.textContent.replace(/#$/, `#${selected.textContent.trimStart()}`);
+                const selection = window.getSelection();
+                const range = document.createRange();
+                selection.removeAllRanges();
+                range.selectNodeContents(inputBox);
+                range.collapse(false);
+                selection.addRange(range);
+                return inputBox.focus();
+            }
+        }
+        
         if (e.key === 'Enter' && inputBox.textContent.trim()) {
             mentionBox.visible(false);
             createMessage(false, inputBox.textContent.trim());
@@ -316,6 +368,8 @@ function createMessage(isBot, content, parseHTML = true) {
     content = content
     .replace(new RegExp(`@${data.bot.username}`, 'g'), `<discord-mention>${data.bot.username}</discord-mention>`)
     .replace(new RegExp(`@${data.user.username}`, 'g'), `<discord-mention highlight>${data.user.username}</discord-mention>`)
+    .replace(new RegExp(`#${_.channels.text}`, 'g'), `<discord-mention type="channel">${_.channels.text}</discord-mention>`)
+    .replace(new RegExp(`#${_.channels.voice}`, 'g'), `<discord-mention type="channel">${_.channels.voice}</discord-mention>`)
 
     if (!isBot) message.textContent = contentRaw;
     if (isBot && !parseHTML) message.textContent = contentRaw;
